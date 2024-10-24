9bd19e4fcddc4205737287c71368523d186fa2c0
dashawnpaige27 <dap6024@psu.edu>
2024-10-24 12:50:17 -0400

    chnages

--- nasa-card.js
+++ nasa-card.js
@@ -8,14 +8,23 @@ import { I18NMixin } from "@haxtheweb/i18n-manager/lib/I18NMixin.js";
 
 /**
  * `nasa-card`
- * 
+ *
  * @demo index.html
  * @element nasa-card
  */
 export class nasaCard extends DDDSuper(I18NMixin(LitElement)) {
-
   static get tag() {
     return "nasa-card";
+  }
+
+  static get properties() {
+    return {
+      ...super.properties,
+      title: { type: String },
+      loading: { type: Boolean, reflect: true },
+      items: { type: Array },
+      value: { type: String },
+    };
   }
 
   constructor() {
@@ -29,47 +38,39 @@ export class nasaCard extends DDDSuper(I18NMixin(LitElement)) {
     this.registerLocalization({
       context: this,
       localesPath:
-        new URL("./locales/nasa-card.ar.json", import.meta.url).href +
-        "/../",
+        new URL("./locales/nasa-card.ar.json", import.meta.url).href + "/../",
       locales: ["ar", "es", "hi", "zh"],
     });
   }
 
-  // Lit reactive properties
-  static get properties() {
-    return {
-      ...super.properties,
-      title: { type: String },
-    };
-  }
-
   // Lit scoped styles
   static get styles() {
-    return [super.styles,
-    css`
-      :host {
-        display: block;
-        color: var(--ddd-theme-primary);
-        background-color: var(--ddd-theme-accent);
-        font-family: var(--ddd-font-navigation);
-      }
-      .wrapper {
-        margin: var(--ddd-spacing-2);
-        padding: var(--ddd-spacing-4);
-      }
-      h3 span {
-        font-size: var(--nasa-card-label-font-size, var(--ddd-font-size-s));
-      }
-    `];
+    return [
+      super.styles,
+      css`
+        :host {
+          display: block;
+          color: var(--ddd-theme-primary);
+          background-color: var(--ddd-theme-accent);
+          font-family: var(--ddd-font-navigation);
+        }
+        .wrapper {
+          margin: var(--ddd-spacing-2);
+          padding: var(--ddd-spacing-4);
+        }
+        h3 span {
+          font-size: var(--nasa-card-label-font-size, var(--ddd-font-size-s));
+        }
+      `,
+    ];
   }
 
   // Lit render the HTML
   render() {
-    return html`
-<div class="wrapper">
-  <h3><span>${this.t.title}:</span> ${this.title}</h3>
-  <slot></slot>
-</div>`;
+    return html` <div class="wrapper">
+      <h3><span>${this.t.title}:</span> ${this.title}</h3>
+      <slot></slot>
+    </div>`;
   }
 
   /**
